a3de9bd04d83bf64a3db1e72edf997aca000caab
Amirthajayabal <165795164+Amirthajayabal@users.noreply.github.com>
2025-08-02 10:04:49 +0530

    Time: 35 ms (13.47%), Space: 46.2 MB (23.58%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        HashSet<Integer>res=new HashSet<>();
+        int candytype=0;
+        for(int n:candyType){
+            if(!res.contains(n)){
+                candytype++;
+                res.add(n);
+            }
+        }
+        int ans=candyType.length/2;
+        return Math.min(candytype,ans);
+    }
+}
