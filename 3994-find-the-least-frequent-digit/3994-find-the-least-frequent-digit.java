d94fae543c755efd3667bf049434b3b05bf4c7d6
Amirthajayabal <165795164+Amirthajayabal@users.noreply.github.com>
2025-10-10 11:41:56 +0530

    Time: 1 ms (97.84%), Space: 40.9 MB (88.89%) - LeetHub

---
+++ 3994-find-the-least-frequent-digit/3994-find-the-least-frequent-digit.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int getLeastFrequentDigit(int n) {
+        int[]freq=new int[10];
+        while(n>0){
+            int dig=n%10;
+            freq[dig]++;
+            n/=10;
+        }
+        int minfreq=Integer.MAX_VALUE;
+        int least=-1;
+        for(int i=0;i<10;i++){
+            if(freq[i]>0 && freq[i]<minfreq){
+                minfreq=freq[i];
+                least=i;
+            }
+        }
+        return least;
+    }
+}
